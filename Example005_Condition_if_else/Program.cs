49bbd74c87a5038235f9873e90b96ff229438409
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-22 20:53:07 +0300

    add Example 005 Condition if-else

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+  Console.WriteLine("Ура. это же Миша!");
+}
+else
+{
+  Console.Write("Привет,");
+  Console.WriteLine(username);
+}
