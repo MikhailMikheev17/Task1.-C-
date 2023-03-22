41e9cd5f50a42798272a14c6fb201fe947f724a1
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-22 20:24:26 +0300

    updated example 003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -5,3 +5,9 @@ Console.WriteLine(numberA + numberB);
 
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+int numberF = new Random().Next(1,10);// 1 2 3... 9, НЕ 10
+Console.WriteLine(numberF);
+int numberG = new Random().Next(1,10);
+Console.WriteLine(numberG);
+Console.WriteLine(numberF + numberG);
