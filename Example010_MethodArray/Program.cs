648b4002490d45cb571691b3e26947e198952e99
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-25 14:46:12 +0300

    add Example 010 MEthodArry

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int[] array = { 11, 23, 35, 4, 45, 66, 77, 4 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < n) 
+{
+
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
